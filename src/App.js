41000cf2c1e0585cddf83e84c46ecf48b5cf9ac6
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-09-29 22:06:45 -0500

    Versión inicial, solo maquetación.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
+
+const titulo = 'Tinto Tareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor-principal">
+      <div className="container-fluid">
+        {/* F1 - Titulo */}
+        <div className="row">
+          <div className="col-12">
+            <h1 className="text-primary text-center">{titulo}</h1>
+          </div>
+        </div>
+
+        {/* F2 - Formulario */}
+        <div className="row">
+          <div className="col-12">
+            <form className="d-flex">
+              <input type="text" className="form-control" id="inlineFormInputGroupTarea" placeholder="Descripción de la tarea..." />
+
+              <button type="submit" className="btn btn-primary">Agregar</button>
+            </form>
+          </div>
+        </div>
+
+        {/* F3 - Lista de Tareas */}
+        <div className="row">
+          <div className="col-12">
+            <table className="table lista-tareas">
+              <tbody>
+                <tr>
+                  <td>
+                    <input type="checkbox" className="form-check-input" />
+                  </td>
+                  <td>
+                    <h4 className="text-primary mb-0">Tarea</h4>
+                    <span className="text-muted">Completado</span>
+                  </td>
+                  <td className="text-end">
+                    <button className="btn btn-link" title="Eliminar">
+                      <span className="material-icons text-danger">
+                        delete
+                      </span>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <input type="checkbox" className="form-check-input" />
+                  </td>
+                  <td>
+                    <h4 className="text-primary mb-0">Tarea</h4>
+                    <span className="text-muted">Completado</span>
+                  </td>
+                  <td className="text-end">
+                    <button className="btn btn-link" title="Eliminar">
+                      <span className="material-icons text-danger">
+                        delete
+                      </span>
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        {/* F4 - Filtros */}
+        <div className="row">
+          <div className="col-12 d-flex justify-content-between align-items-center">
+            <div>
+              <button className="btn btn-primary btn-sm me-1">Todas</button>
+              <button className="btn btn-outline-primary btn-sm me-1">Pendientes</button>
+              <button className="btn btn-outline-primary btn-sm">Completadas</button>
+            </div>
+
+            <button className="btn btn-link btn-sm">
+              <span className="material-icons">
+                clear
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
