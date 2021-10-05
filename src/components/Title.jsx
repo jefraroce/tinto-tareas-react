a61ceeb767e35b95e9b42fdc113d55040fbf3b7b
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-10-04 22:05:17 -0500

    Creado componente Title para demostrar el uso de props.

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+function Title(props) {
+  return (
+    <div className="row">
+      <div className="col-12">
+        <h1 className="text-primary text-center">{props.title}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
